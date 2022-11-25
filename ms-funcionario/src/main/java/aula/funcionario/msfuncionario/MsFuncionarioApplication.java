d318fa9576f74f1be6b530db04e394dae945a4c6
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-11-25 12:03:56 -0300

    Connected all services to Eureka in separate ports.

--- ms-funcionario/src/main/java/aula/funcionario/msfuncionario/MsFuncionarioApplication.java
+++ ms-funcionario/src/main/java/aula/funcionario/msfuncionario/MsFuncionarioApplication.java
@@ -2,8 +2,10 @@ package aula.funcionario.msfuncionario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsFuncionarioApplication {
 
     public static void main(String[] args) {
